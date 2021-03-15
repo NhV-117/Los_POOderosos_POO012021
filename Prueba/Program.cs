932e86933c433b65736a12c60036cc45ef84c454
NhV-117 <00143720@uca.edu.sv>
2021-03-14 22:44:24 -0600

    example 1

---
+++ Prueba/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Prueba
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int a = 0, b=0;
+
+            Console.Write("Enter a value for a: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter a value for b: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals {sum(a,b)}");
+        }
+
+        public static int sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
